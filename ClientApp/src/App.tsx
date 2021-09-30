2fd752204b05777dba623d06ab816baa0cf844d4
Mory <mouradgaaloul26@gamil.com>
2021-10-01 00:53:19 +0200

    initial Setup

--- ClientApp/src/App.tsx
+++ ClientApp/src/App.tsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router';
 import { Layout } from './components/Layout';
 import { Home } from './components/Home';
 import { FetchData } from './components/FetchData';
@@ -12,11 +11,7 @@ export default class App extends Component {
 
   render () {
     return (
-      <Layout>
-        <Route exact path='/' component={Home} />
-        <Route path='/counter' component={Counter} />
-        <Route path='/fetch-data' component={FetchData} />
-      </Layout>
+      <h1>Ecommerce Webapp</h1>
     );
   }
 }
