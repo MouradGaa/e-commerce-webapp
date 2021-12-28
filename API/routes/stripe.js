c8acfc4aa167d08bba1872d121d3255bce7d4b02
MouradGaa <62558441+MouradGaa@users.noreply.github.com>
2021-12-28 11:19:32 +0100

    Update stripe.js

--- API/routes/stripe.js
+++ API/routes/stripe.js
@@ -1,7 +1,7 @@
 
 const router = require('express').Router();
 
-const stripe = require('stripe')('sk_test_51JzyQhJotT9QPNrJEDW4th85B1SC3K33TugUIDVgvKvAGtt60zsdu7xXFXOXqDIDK8o754C7q35w7Xynx5yfQUrT00DkRKiCqN');
+const stripe = require('stripe')();
 
 
 
